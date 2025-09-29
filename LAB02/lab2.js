d07ffaafd54faad5caba7a7f29c645f3bfe59b9e
mariammueenie <156461593+mariammueenie@users.noreply.github.com>
2025-09-28 23:07:29 -0400

    Update lab2.js
    Reordered steps to be executed, to make sure game logic is sound and does not crash terminal by repeating multiple times :(.
    
    Rewrote the comments to describe the rest of the games logic.

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -1,8 +1,28 @@
-// start game
+// LAB02 - Rock, Paper, Scissors Game
+
+// Step 1: Start game.
 var prompt = require('prompt');
 prompt.start();
 
-// Instruction #3
+// Step 2: Generate computer choice using Math.random()
+    // where; 
+    // 0.00-0.34 = PAPER
+    // 0.35-0.67 = SCISSORS
+    // 0.68-1.00 = ROCK
+function getComputerChoice() {
+    const randomNum = Math.random(); // generated random number between 0-1
+
+    // determine computer choice based on random number
+    if (randomNum <= 0.34) {
+        return 'paper';
+    } else if (randomNum <= 0.67) {
+        return 'scissors';
+    } else {
+        return 'rock';
+    }
+}
+
+// Step 3: Ask user for their input, using prompt().
 // Get user to choose between rock, paper, or scissors
 // Response will be stored as userSelection 
 // makes it so that user can only choose rock, paper, or scissors
@@ -13,37 +33,18 @@ prompt.get([{
     message: 'Input must be rock, paper, or scissors',
     required: true
 }], function (err, result) {
-    if (err) { return onErr(err); }
-    console.log('Command-line input received:');
-    console.log('You chose: ' + result.userSelection);
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    // Retrieve userSelection and ComputerChoice
+
+    // Print userSelection and ComputerChoice using console.log()
+
+    // Compare userSelection with ComputerChoise to determine winner, and display results.
 });
 
-// Instruction #4
-// Generate computer choice using Math.random(), 
-// where 0.00-0.34 = PAPER
-// 0.35-0.67 = SCISSORS
-// 0.68-1.00 = ROCK
-function getComputerChoice() {
-    const randomNum = Math.random(); // generated random number between 0-1
-
-    // determine computer choice based on random number
-    if (randomNum < 0.34) {
-        return 'paper';
-    } else if (randomNum <= 0.67) {
-        return 'scissors';
-    } else {
-        return 'rock';
-    }
-}
-
-// Instruction #5
-// Display userSelection and computerSelection using console.log()
-const computerSelection = getComputerChoice();
-console.log('Computer chose: ' + computerSelection);
-const userSelection = result.userSelection.toLowerCase(); // convert user input to lowercase for comparison
 
 
-// Compare userSelection with computerSelection
-
-// Display results of game (ie: You chose X, Computer chose Y, You Win/Lose/Tie)
 
