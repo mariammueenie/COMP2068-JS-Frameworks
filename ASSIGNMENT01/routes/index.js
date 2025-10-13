a7c688004041c49b587b15e92bc72b340b2e6da1
mariammueenie <156461593+mariammueenie@users.noreply.github.com>
2025-10-13 19:14:57 -0400

    Update index.js
    Fixed get home page route

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,35 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Home
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+// About
+router.get('/about', (req, res) => {
+  res.render('about', { title: 'About Me' });
+});
+
+// Projects
+router.get('/projects', (req, res) => {
+  res.render('projects', { title: 'Projects' });
+});
+
+// Contact
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
+
