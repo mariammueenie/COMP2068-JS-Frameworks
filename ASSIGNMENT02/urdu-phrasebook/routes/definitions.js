1ce1ec619da262acb883e725ed141815019f0658
mariammueenie <mariam_mueen@live.ca>
2025-12-05 22:54:15 -0500

    CRUD Added, Styling
    Create definitions added
    - /views/definitions/add.hbs
    - /routes/definitions.js
    Read defintions added
    - /views/definitions/search.hbs
    - /routes/definitons.js
    
    Styling
    - Basic bootstrap styling for navlinks, homepage
    - Functional views w minimal styling

--- ASSIGNMENT02/urdu-phrasebook/routes/definitions.js
+++ ASSIGNMENT02/urdu-phrasebook/routes/definitions.js
@@ -4,6 +4,68 @@ const { body, validationResult } = require('express-validator');
 const Definition = require('../models/Definition');
 
 // ========================================
+// Get /definitions/search 
+// To search definitions by English term
+// ========================================
+// GET /definitions/search
+router.get('/search', async (req, res) => {
+  const q = req.query.q || "";
+
+  try {
+    const results = await Definition.find({
+      english: { $regex: q, $options: "i" }
+    });
+
+    res.render('definitions/search', { q, results });
+
+  } catch (err) {
+    res.status(500).send("Error searching definitions");
+  }
+});
+
+//=========================================
+// GET /definitions/edit/:id -> Show Edit Form
+//========================================
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const definition = await Definition.findById(req.params.id);
+    res.render('definitions/edit', { definition });
+  } catch (err) {
+    res.status(500).send("Error loading edit page");
+  }
+});
+
+//=========================================
+// POST /definitions/edit/:id -> Update definition
+//========================================
+router.post('/edit/:id', async (req, res) => {
+  try {
+    await Definition.findByIdAndUpdate(req.params.id, {
+      english: req.body.english,
+      urdu: req.body.urdu,
+      example: req.body.example
+    });
+
+    res.redirect('/definitions');
+
+  } catch (err) {
+    res.status(500).send("Error updating definition");
+  }
+});
+
+//=========================================
+// GET /definitions/delete/:id -> Delete definition
+//=======================================
+router.get('/delete/:id', async (req, res) => {
+  try {
+    await Definition.findByIdAndDelete(req.params.id);
+    res.redirect('/definitions');
+  } catch (err) {
+    res.status(500).send("Error deleting item");
+  }
+});
+
+// ========================================
 // GET /definitions  -> List all definitions
 // ========================================
 router.get('/', async (req, res) => {
