e9b27bb773fbdfc7223563d58c3b375ff4e38b3e
mariammueenie <mariam_mueen@live.ca>
2026-02-25 15:12:03 -0500

    Debug: Database connections

--- ASSIGNMENT02/config/db.js
+++ ASSIGNMENT02/config/db.js
@@ -5,14 +5,20 @@ function connectDB() {
   const uri = process.env.MONGODB_URI;
 
   if (!uri) {
-    console.error('❌ MONGODB_URI not set in .env');
+    console.error('MONGODB_URI not set in environment variables');
     process.exit(1);
   }
 
   mongoose.connect(uri)
-    .then(() => console.log('✅ MongoDB connected'))
+    .then(() => {
+      console.log('MongoDB connected');
+
+      // DEBUG: confirm EXACT DB + host this deployment is using
+      console.log('DB NAME:', mongoose.connection.name);
+      console.log('DB HOST:', mongoose.connection.host);
+    })
     .catch((err) => {
-      console.error('❌ MongoDB connection error:', err);
+      console.error('MongoDB connection error:', err);
       process.exit(1);
     });
 }
